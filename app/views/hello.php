1e7fb8f5bbf3af953f1e69949af2e4375e2f26ab
David Furnes <dfurnes@dosomething.org>
2014-09-15 15:52:58 -0400

    Add editorconfig with Laravel's tab spacing convention.

--- app/views/hello.php
+++ app/views/hello.php
@@ -4,16 +4,16 @@
 	<meta charset="UTF-8">
 	<title>Voting App</title>
 
-  <link rel="stylesheet" href="/bower_components/neue/dist/neue.css" type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="/bower_components/neue/dist/neue.css" type="text/css" media="screen" charset="utf-8">
 </head>
 <body>
   <div class="chrome">
-    <div class="container">
-      <div class="wrapper">
-        <h1>You have arrived.</h1>
-        <p>Voting for web artisans.</p>
-      </div>
-    </div>
+	<div class="container">
+	  <div class="wrapper">
+		<h1>You have arrived.</h1>
+		<p>Voting for web artisans.</p>
+	  </div>
+	</div>
   </div>
 
   <script src="/bower_components/jquery/dist/jquery.min.js" type="text/javascript" charset="utf-8"></script>
