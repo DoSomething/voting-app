d380cfc5f0502cf3511a8e8f910bd11c269878e3
David Furnes <david@dfurnes.com>
2014-10-24 11:20:08 -0400

    A wee bit of CSS.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,23 +2,28 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Voting [4.2]</title>
+	<title>Celebs Gone Laravel</title>
 
 	<link rel="stylesheet" href="/dist/app.css" type="text/css" media="screen" charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
 </head>
 <body>
   <div class="chrome">
   <div class="chrome--wrapper">
+		<nav class="chrome--nav">
+			<a class="logo" href="http://www.dosomething.org"><img src="/logo.png" alt="Celebs Gone Good"></a>
+			<a class="hamburger js-toggle-mobile-menu" href="#">&#xe606;</a>
+			<div class="menu">
+				<ul class="primary-nav">
+					<li>{{ link_to_route('candidates.index', 'Candidates') }}</li>
+					<li>{{ link_to_route('categories.index', 'Categories') }}</li>
+				</ul>
+			</div>
+		</nav>
+
     <div class="container">
       <div class="wrapper">
-        <h1>Celebs Gone Laravel</h1>
-				<nav class="tabs">
-				<ul>
-				<li>{{ link_to_route('candidates.index', 'Candidates') }}</li>
-				<li>{{ link_to_route('categories.index', 'Categories') }}</li>
-				</ul>
-
-				</nav>
         @yield('content')
       </div>
     </div>
