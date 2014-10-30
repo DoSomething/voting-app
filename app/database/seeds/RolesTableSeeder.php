8f39144e3711c3b222e0ffbf9b6e7599a33f1768
Andrea Gaither <angaither@gmail.com>
2014-10-30 16:22:15 -0400

    Create admin role with a seed.

---
+++ app/database/seeds/RolesTableSeeder.php
@@ -0,0 +1,13 @@
+<?php
+class RolesTableSeeder extends Seeder {
+  /**
+   * Seed the Roles table.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Role::truncate();
+    Role::create(['name' => 'admin']);
+  }
+}
