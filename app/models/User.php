03c595e656f6e3b7ac094ae0d47e947da55c916c
Andrea Gaither <angaither@gmail.com>
2014-11-11 09:41:45 -0500

    Removes remindable password trait/interface.

--- app/models/User.php
+++ app/models/User.php
@@ -2,12 +2,10 @@
 
 use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface{
 
-  use UserTrait, RemindableTrait;
+  use UserTrait;
 
   /**
    * The attributes which may be mass-assigned.
@@ -21,7 +19,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
    *
    * @var array
    */
-  protected $hidden = ['password', 'remember_token'];
+  protected $hidden = ['password'];
 
   /**
    * Mutator to hash the password for safe storage.
