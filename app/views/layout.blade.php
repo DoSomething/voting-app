51e4dd007d7679965da0abab0a5b65c7a1ac3bf7
David Furnes <david@dfurnes.com>
2014-11-11 16:47:44 -0500

    Add highlighted active class helper for main navigation.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -18,6 +18,7 @@
       <a class="logo" href="{{{ route('home') }}}"><img src="/dist/images/logo.png" alt="Celebs Gone Good"></a>
       <ul>
         @forelse(Category::all() as $category)
+        <li>{{ highlighted_link_to_route('categories.show', $category->name, [$category->slug]) }}</li>
         @empty
         <li>No categories.</li>
         @endforelse
