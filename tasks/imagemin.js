38dd27558753a1b2fa8ca72955877eaa9ea95b0f
David Furnes <david@dfurnes.com>
2014-11-11 16:44:13 -0500

    Update grunt tasks to copy fonts, and copy images on initial build.

--- tasks/imagemin.js
+++ tasks/imagemin.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var changed = require('gulp-changed');
 var imagemin = require('gulp-imagemin');
 
+var DEST = './public/dist/images';
+
 gulp.task('imagemin', function() {
 	return gulp.src('./app/assets/images/**')
-		.pipe(changed(dest)) // Ignore unchanged files
+		.pipe(changed(DEST)) // Ignore unchanged files
 		.pipe(imagemin())
-		.pipe(gulp.dest('./public/dist/images'));
+		.pipe(gulp.dest(DEST));
 });
--- tasks/watch.js
+++ tasks/watch.js
@@ -3,4 +3,5 @@ var gulp = require('gulp');
 gulp.task('watch', ['browserify-watch'], function() {
 	gulp.watch('app/assets/stylesheets/**', ['sass']);
 	gulp.watch('app/assets/images/**', ['imagemin']);
+	gulp.watch('app/assets/fonts/**', ['copy']);
 });
