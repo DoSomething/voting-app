6955c83ffcc8be41ba5e7b142d0f5496995c2ff8
David Furnes <david@dfurnes.com>
2014-11-12 11:37:50 -0500

    Add setting for changing footer link text/URL.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -39,7 +39,7 @@
       <ul class="footer__links">
         <li><a class="footer__link -facebook" href="#"><span>Facebook</span></a></li>
         <li><a class="footer__link -twitter" href="#"><span>Twitter</span></a></li>
-        <li><a  href="#">FAQs</a></li>
+        <li><a  href="{{ $settings['faq_link_url'] }}">{{ $settings['faq_link_text'] }}</a></li>
       </ul>
     </footer>
 
