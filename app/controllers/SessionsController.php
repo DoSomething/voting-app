931600ec5b6b18264636a6a5aaa90b357a8745a5
Andrea Gaither <angaither@gmail.com>
2014-11-12 11:49:01 -0500

    Create new route for admin login.

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -10,28 +10,68 @@ class SessionsController extends \BaseController {
   }
 
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /sessions/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
+  /**
+   * Show the form for creating a new resource.
+   * GET /sessions/create
+   *
+   * @return Response
+   */
+  public function create()
+  {
+    // @TODO: switch email/phone based on IP.
     return View::make('sessions.create');
-	}
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /sessions
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-    $input = Input::only('first_name', 'email', 'birthdate');
-    $this->sessionValidator->validate($input);
+  /**
+   * Allows an admin to log in.
+   *
+   */
+  public function adminCreate()
+  {
+    return View::make('sessions.admin-create');
 
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   * POST /sessions
+   *
+   * @return Response
+   */
+  public function store()
+  {
+    // If coming from admin, use that login method.
+    if (strpos($_SERVER['HTTP_REFERER'], 'admin')) {
+      $input = Input::only('email', 'password');
+      return $this->adminLogin($input);
+    }
+    // Use the user login/create method.
+    else {
+      $input = Input::only('first_name', 'email', 'birthdate');
+      $this->sessionValidator->validate($input);
+
+      return $this->userLogin($input);
+    }
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   * DELETE /sessions/{id}
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id = null)
+  {
+    Auth::logout();
+
+    return Redirect::home()->withFlashMessage('You\'re now signed out.');;
+  }
+  /**
+   *
+   */
+  public function userLogin($input)
+  {
     $user = User::isCurrentUser($input);
     if (!$user) {
       $user = User::createNewUser($input);
@@ -39,20 +79,21 @@ class SessionsController extends \BaseController {
     // Log in the user.
     Auth::login($user);
     return Redirect::intended('/')->withFlashMessage('Welcome ' . $input['first_name']);
-	}
+  }
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /sessions/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id = null)
-	{
-    Auth::logout();
+  /**
+   *
+   */
+  public function adminLogin($input)
+  {
+    // $this->sessionValidator->validate($input);
+    if(Auth::attempt($input)) {
+      return Redirect::intended('/')->withFlashMessage('Welcome back!');
+    }
+    else {
+      return Redirect::back()->withInput()->withFlashMessage('Invalid username or password!');
+    }
 
-    return Redirect::home()->withFlashMessage('You\'re now signed out.');;
-	}
+  }
 
 }
---
+++ app/views/sessions/admin-create.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('content')
+  <h3>Sign In</h3>
+
+  {{ Form::open(['route'=> ['sessions.store'], 'id' => 'sign_in_form']) }}
+
+    {{ Form::label('email', 'Email') }}
+    {{ Form::error('email', $errors) }}
+    {{ Form::text('email') }}
+
+    {{ Form::label('password', 'Password') }}
+    {{ Form::error('password', $errors) }}
+    {{ Form::password('password') }}
+
+    {{ Form::submit('Sign In', ['class' => 'btn']) }}
+  {{ Form::close() }}
+
+@stop
