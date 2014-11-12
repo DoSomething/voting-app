4e3ebd9b16ffe5dfca00d273cb781a23adc481e0
Andrea Gaither <angaither@gmail.com>
2014-11-12 10:36:40 -0500

    Make phone, email unique. Fixes #51

---
+++ app/database/migrations/2014_11_12_153219_unique-email-phone.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UniqueEmailPhone extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('users', function(Blueprint $table)
+    {
+      $table->unique('email');
+      $table->unique('phone');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('users', function(Blueprint $table)
+    {
+      $table->dropUnique('users_email_unique');
+      $table->dropUnique('users_phone_unique');
+    });
+  }
+
+}
