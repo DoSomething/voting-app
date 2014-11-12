8ae86c5a0794dcdfdc749939c3090c91ed515df4
David Furnes <david@dfurnes.com>
2014-11-12 11:40:57 -0500

    Only show admin menu to admins.

--- app/views/partials/admin.blade.php
+++ app/views/partials/admin.blade.php
@@ -1,5 +1,5 @@
+@if(Auth::user() && Auth::user()->hasRole('admin'))
 <footer class="admin">
-  @if(Auth::user() && Auth::user()->hasRole('admin'))
   <div class="column">
     <h4>Administration</h4>
     <ul>
@@ -10,26 +10,19 @@
         <li>{{ link_to_route('settings.index', 'Site Settings') }}</li>
     </ul>
   </div>
-  @endif
 
   <div class="column">
     <h4>User</h4>
     <ul>
-      @if(Auth::guest())
-        <li>{{ link_to_route('users.create', 'Create Account') }}</li>
-        <li>{{ link_to_route('login', 'Sign In') }}</li>
-      @else
-        <li>{{ link_to_route('logout', 'Sign Out') }}</li>
-      @endif
+      <li>{{ link_to_route('logout', 'Sign Out') }}</li>
     </ul>
   </div>
 
-  @if(Auth::user() && Auth::user()->hasRole('admin'))
   <div class="column">
     <h4>Actions</h4>
     <ul>
       @yield('actions', 'No actions on this page.')
     </ul>
-    @endif
   </div>
 </footer>
+@endif
