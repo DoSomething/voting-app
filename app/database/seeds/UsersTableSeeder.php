7c585e1eb6ee5ea9f27f44dda054666e6dc783c3
Andrea Gaither <angaither@gmail.com>
2014-11-13 15:38:10 -0500

    Add content peeps to the db.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -20,19 +20,33 @@ class UsersTableSeeder extends Seeder {
       'password' => 'tops3cret',
       ])->assignRole(1);
 
-    foreach(range(1,25) as $index) {
-      User::create([
-        'first_name' => $faker->firstName,
-        'email' => $faker->unique()->safeEmail,
-        'birthdate' => $faker->date($format = 'm/d/Y', $max = 'now'),
-      ]);
-    }
-    foreach(range(1,25) as $index) {
-      User::create([
-        'first_name' => $faker->firstName,
-        'phone' => $faker->unique()->phoneNumber,
-        'birthdate' => $faker->date($format = 'm/d/Y', $max = 'now'),
-      ]);
+    User::create([
+      'first_name' => 'Calvin',
+      'email' => 'cstowell@dosomething.org',
+      'password' => 'tops3cret',
+      ])->assignRole(1);
+
+    User::create([
+      'first_name' => 'Naomi',
+      'email' => 'nhirabayashi@dosomething.org',
+      'password' => 'tops3cret',
+      ])->assignRole(1);
+
+    if (App::environment('local')) {
+      foreach(range(1,25) as $index) {
+        User::create([
+          'first_name' => $faker->firstName,
+          'email' => $faker->unique()->safeEmail,
+          'birthdate' => $faker->dateTime('now'),
+        ]);
+      }
+      foreach(range(1,25) as $index) {
+        User::create([
+          'first_name' => $faker->firstName,
+          'phone' => $faker->unique()->phoneNumber,
+          'birthdate' => $faker->dateTime('now'),
+        ]);
+      }
     }
   }
 }
