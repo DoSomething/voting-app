9d3d7804bd7f443a2647033942ce66300186bf3d
David Furnes <david@dfurnes.com>
2014-11-13 12:50:19 -0500

    Added a simple 404 page.

---
+++ app/views/missing.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+  <div class="wrapper">
+    <h1 class="highlighted">Not found!</h1>
+    <h3>We can't find that page.</h3>
+    <p>Try clicking on one of the categories above...</p>
+  </div>
+@stop
+
