d4ceb787ebf091eaeafaf17005f8536b3d1172ca
Andrea Gaither <angaither@gmail.com>
2014-11-13 10:57:08 -0500

    Removed form::error macros and todo.

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -5,7 +5,7 @@
 
   {{ Form::open(['route'=> ['sessions.store'], 'id' => 'sign_in_form']) }}
     {{ Form::label('first_name', 'First Name') }}
-    {{ Form::error('first_name', $errors) }}
+    {{ form_error('first_name', $errors) }}
     {{ Form::text('first_name') }}
 
     {{--@TODO email or phone number depending on country code--}}
@@ -14,14 +14,8 @@
     {{ Form::text('email') }}
 
     {{ Form::label('birthdate', 'Birthdate') }}
-    {{ Form::error('birthdate', $errors) }}
+    {{ form_error('birthdate', $errors) }}
     {{ Form::text('birthdate') }}
-
-    {{-- @TODO: password login for admins
-    {{ Form::label('password', 'Password') }}
-    {{ form_error('password', $errors) }}
-    {{ Form::password('password') }}
-    --}}
 
     {{ Form::submit('Sign In', ['class' => 'btn']) }}
   {{ Form::close() }}
