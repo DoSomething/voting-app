48d3b40fa95bff0de53ead27ac85ddd33ad24e72
David Furnes <david@dfurnes.com>
2014-11-13 15:55:47 -0500

    Add styles for candidate detail page.

--- app/views/candidates/show.blade.php
+++ app/views/candidates/show.blade.php
@@ -3,48 +3,47 @@
 @section('title', $candidate->name)
 
 @section('content')
-  <h3>{{{ $candidate->name }}}</h3>
+<div class="candidate">
+  <div class="candidate__info">
+    <article class="tile -alternate">
+      <a class="wrapper" href="{{{ route('candidates.show', [$candidate->slug]) }}}">
+        <div class="tile__meta">
+          <h1 class="__title">{{{ $candidate->name }}}</h1>
+        </div>
+        <img alt="{{{ $candidate->name }}}" src="{{{ $candidate->present()->thumbnail }}}" src="" />
+      </a>
+    </article>
 
-  @if($candidate->description)
-  <h4>About this Candidate</h4>
-  <p>{{{ $candidate->description }}}</p>
-  @endif
+    <p>{{{ $candidate->description }}}</p>
+  </div>
 
-  @if(Auth::user() && Auth::user()->hasRole('admin') && $votes)
-  <h4>{{{$vote_count }}} {{{ str_plural('vote', $vote_count)}}}</h4>
-  <ul>
-  @forelse ($votes as $vote)
-    <li>{{ link_to_route('users.show', $vote->user->email, $vote->user->id) }}</li>
-  @empty
-    <li>No votes! :(</li>
-  @endforelse
-  </ul>
-  @endif
-
-  <h4>Your Vote</h4>
-  @if(Auth::user())
-    @if (Auth::user()->canVote($candidate))
-      {{ Form::open(['route' => 'votes.store']) }}
-      {{ Form::hidden('candidate_id', $candidate->id) }}
-      {{ Form::submit('Vote', ['class' => 'btn']) }}
-      {{ Form::close() }}
+  <div class="candidate__actions">
+    @if(Auth::user())
+      @if (Auth::user()->canVote($candidate))
+        <p>Welcome back, {{ Auth::user()->first_name }}! Ready to vote again?</p>
+        {{ Form::open(['route' => 'votes.store']) }}
+        {{ Form::hidden('candidate_id', $candidate->id) }}
+        {{ Form::submit('Count My Vote', ['class' => 'button -primary']) }}
+        {{ Form::close() }}
+      @else
+        You've already voted today!
+      @endif
     @else
-      You've already voted today!
+      {{ link_to_route('login', 'Sign in') }} to vote!
     @endif
-  @else
-    {{ link_to_route('login', 'Sign in') }} to vote!
-  @endif
 
-
-  @if($candidate->photo)
-  <h4>Photo</h4>
-  <a href="/images/{{{ $candidate->photo }}}"><img src="/images/thumb-{{{ $candidate->photo }}}" alt="{{{ $candidate->name }}}" width="200" height="200"></a>
-  @endif
-
-  @if($candidate->category)
-  <h4>Category</h4>
-  <p>{{ link_to_route('categories.show', $candidate->category->name, [$candidate->category->slug]) }}</p>
-  @endif
+    @if(Auth::user() && Auth::user()->hasRole('admin') && $votes)
+    <h4>{{{$vote_count }}} {{{ str_plural('vote', $vote_count)}}}</h4>
+    <ul>
+    @forelse ($votes as $vote)
+      <li>{{ link_to_route('users.show', $vote->user->email, $vote->user->id) }}</li>
+    @empty
+      <li>No votes! :(</li>
+    @endforelse
+    </ul>
+    @endif
+  </div>
+</div>
 @stop
 
 @section('actions')
