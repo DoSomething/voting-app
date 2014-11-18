f691c29a7abeabfced6c94e30688ec3ad197c85a
Andrea Gaither <angaither@gmail.com>
2014-11-18 16:57:22 -0500

    Make cell form match the other form.

--- app/views/sessions/partials/_user_phone.blade.php
+++ app/views/sessions/partials/_user_phone.blade.php
@@ -4,15 +4,15 @@
 
   {{ Form::label('first_name', 'First Name') }}
   {{ form_error('first_name', $errors) }}
-  {{ Form::text('first_name') }}
-
-  {{ Form::label('phone', 'Cell Number') }}
-  {{ form_error('phone', $errors) }}
-  {{ Form::text('phone') }}
+  {{ Form::text('first_name', null, ['placeholder' => 'What\'s your name?']) }}
 
   {{ Form::label('birthdate', 'Birthdate') }}
   {{ form_error('birthdate', $errors) }}
   {{ Form::text('birthdate',  null, ['placeholder' => 'MM/DD/YYYY']) }}
+
+  {{ Form::label('phone', 'Cell Number') }}
+  {{ form_error('phone', $errors) }}
+  {{ Form::text('phone') }} {{-- @TODO: add placeholder--}}
 
   {{ Form::submit('Sign In', ['class' => 'btn']) }}
 {{ Form::close() }}
