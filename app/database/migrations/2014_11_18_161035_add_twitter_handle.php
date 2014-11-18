522cc4952d13808e49b61c9aae30a8e054ba06f4
Andrea Gaither <angaither@gmail.com>
2014-11-18 11:15:13 -0500

    Twitter handle migration.

---
+++ app/database/migrations/2014_11_18_161035_add_twitter_handle.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTwitterHandle extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('candidates', function(Blueprint $table)
+    {
+      $table->string('twitter')->after('photo');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('candidates', function(Blueprint $table)
+    {
+      $table->dropColumn('twitter');
+    });
+  }
+
+}
