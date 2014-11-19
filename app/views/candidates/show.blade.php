1cede2e944c26db1e4ef8fad092014f547129f63
Andrea Gaither <angaither@gmail.com>
2014-11-19 15:15:28 -0500

    Added candidate delete to remove duplicates. Fixes #122

--- app/views/candidates/show.blade.php
+++ app/views/candidates/show.blade.php
@@ -32,5 +32,8 @@
 @section('actions')
   @if(Auth::user() && Auth::user()->hasRole('admin'))
     <li>{{ link_to_route('candidates.edit', 'Edit Candidate', [$candidate->slug], ['class' => 'btn secondary']) }}</li>
+    {{ Form::open(['route'=> ['candidates.update', $candidate->slug], 'method' => 'delete']) }}
+          {{ Form::submit('Delete Candidate', ['class' => 'btn btn-danger']) }}
+    {{ Form::close() }}
   @endif
 @stop
