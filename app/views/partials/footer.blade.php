69283a9f19b8d0ba86d23049ee0d8e6dd442fa4e
David Furnes <david@dfurnes.com>
2014-11-21 10:58:10 -0500

    Add site share links in footer.

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -2,8 +2,8 @@
   {{ $settings['site_title'] }} is a program of <a href="https://www.dosomething.org">DoSomething.org</a>.
 
   <ul class="footer__links">
-    <li><a class="social-icon -facebook" href="#"><span>Facebook</span></a></li>
-    <li><a class="social-icon -twitter" href="#"><span>Twitter</span></a></li>
+    <li><a class="social-icon -facebook js-share-link" href="{{ facebook_intent(url('/')) }}"><span>Facebook</span></a></li>
+    <li><a class="social-icon -twitter js-share-link" href="{{ tweet_intent('Vote for your favorite celeb who has done kick ass things in the world in ' . $settings['site_title' ]. '.', url('/')) }}"><span>Twitter</span></a></li>
     <li><a  href="{{ $settings['faq_link_url'] }}">{{ $settings['faq_link_text'] }}</a></li>
   </ul>
 </footer>
