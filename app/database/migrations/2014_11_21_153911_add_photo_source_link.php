e2f8d576e21002803496180bb8a35d612ef6c322
Andrea Gaither <angaither@gmail.com>
2014-11-21 10:43:11 -0500

    Photo source migration.

---
+++ app/database/migrations/2014_11_21_153911_add_photo_source_link.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPhotoSourceLink extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('candidates', function(Blueprint $table)
+    {
+      $table->string('photo_source')->after('photo');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('candidates', function(Blueprint $table)
+    {
+      $table->dropColumn('photo_source');
+    });
+  }
+
+}
