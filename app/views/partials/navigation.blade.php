35a6745563f0018ace57b621ad2ee5b5a61d456f
David Furnes <david@dfurnes.com>
2014-12-03 10:39:52 -0500

    "Kick ass" → "Kickass". :punch:

--- app/views/partials/navigation.blade.php
+++ app/views/partials/navigation.blade.php
@@ -4,7 +4,7 @@
   @if(Session::has('flash_message'))
     <div id="message" class="messages {{ Session::get('flash_message_type', '') }}">{{ Session::get('flash_message') }}</div>
   @else
-    <p class="navigation__tagline">Vote for your favorite celeb who has done kick ass things in the world. Vote once per day in each category, and don't forget voting ends December 24th!</p>
+    <p class="navigation__tagline">Vote for your favorite celeb who has done kickass things in the world. Vote once per day in each category, and don't forget voting ends December 24th!</p>
   @endif
 
   <ul>
