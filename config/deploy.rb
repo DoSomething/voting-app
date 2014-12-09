583823d79bbcabbc211580d19f62d46501f97dc2
David Furnes <david@dfurnes.com>
2014-12-09 10:56:04 -0500

    Add Capistrano task to clear cache on deploys.

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,8 +33,13 @@ namespace :deploy do
     run "cd #{release_path} && php artisan migrate --force"
   end
 
+  task :artisan_cache_clear do
+    run "cd #{release_path} && php artisan cache:clear"
+  end
+
 end
 
 after "deploy:update", "deploy:cleanup"
 after "deploy:symlink", "deploy:link_folders"
 after "deploy:link_folders", "deploy:artisan_migrate"
+after "deploy:artisan_migrate", "deploy:artisan_cache_clear"
