459e1371a8ee442b4accaf621578db9431b5b922
David Furnes <david@dfurnes.com>
2014-12-16 12:34:07 -0500

    Add comments to make vote form partial more readable.

--- app/views/candidates/voteForm.blade.php
+++ app/views/candidates/voteForm.blade.php
@@ -1,21 +1,28 @@
 @if(Auth::user())
+{{-- If user is logged in... --}}
+
+  {{--  ...and can vote, show the voting form.  --}}
   @if (Auth::user()->canVoteInCategory($category))
     <p class="heading -hero">Hey, {{ Auth::user()->first_name }}! Ready to cast your vote for {{{ $category->name }}}?</p>
     {{ Form::open(['route' => 'votes.store']) }}
     {{ Form::hidden('candidate_id', (isset($id) ? $id : null)) }}
     {{ Form::submit('Count My Vote', ['class' => 'button -primary']) }}
     {{ Form::close() }}
+
+  {{-- ...and already voted, display a message. --}}
   @else
     <p class="heading -alpha">Thanks for voting! You can vote again in this category in 24 hours.</p>
     <p class="heading -gamma">Get {{ $candidate->name or "CANDIDATE_NAME" }} more votes!</p>
     @include('candidates.share');
   @endif
+
+{{-- If user is not logged in, show the login/vote form. --}}
 @else
     {{ Form::open(['route'=> ['sessions.store'], 'id' => 'sign_in_form']) }}
       @include('sessions.form', [$type])
       {{ Form::hidden('candidate_id', (isset($candidate->id) ? $candidate->id : null)) }}
       {{ Form::submit('Count My Vote', ['class' => 'button -primary']) }}
-      <p class="legal" id="ctia">By voting you agree to receive future updates from DoSomething.org. Message & data rates may apply. Text STOP to opt-out, HELP for help.</p>
+      <p class="legal" id="ctia">By voting you agree to receive future updates from DoSomething.org. Message &amp; data rates may apply. Text STOP to opt-out, HELP for help.</p>
     {{ Form::close() }}
 @endif
 
