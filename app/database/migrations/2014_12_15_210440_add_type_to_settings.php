a85b637072f7529de00e40e65d850bdac4ca0e9e
David Furnes <david@dfurnes.com>
2014-12-16 10:41:41 -0500

    Fix indentation in migration.

--- app/database/migrations/2014_12_15_210440_add_type_to_settings.php
+++ app/database/migrations/2014_12_15_210440_add_type_to_settings.php
@@ -5,33 +5,33 @@ use Illuminate\Database\Schema\Blueprint;
 
 class AddTypeToSettings extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('settings', function(Blueprint $table)
-		{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('settings', function(Blueprint $table)
+    {
       $table->string('type')->default('text');
       $table->text('description')->nullable();
-		});
-	}
+    });
+  }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('settings', function(Blueprint $table)
-		{
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('settings', function(Blueprint $table)
+    {
       $table->dropColumn('description');
       $table->dropColumn('type');
-		});
-	}
+    });
+  }
 
 }
