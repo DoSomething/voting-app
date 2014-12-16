36dd39df2360239fd350494d621e76ef77314e95
David Furnes <david@dfurnes.com>
2014-12-16 10:35:14 -0500

    Update setting form to handle boolean settings.

--- app/views/settings/edit.blade.php
+++ app/views/settings/edit.blade.php
@@ -12,7 +12,21 @@
     </div>
 
     {{ Form::model($setting, ['route'=> ['settings.update', $setting->key], 'method' => 'PATCH']) }}
-      @include('settings.form')
+
+      @if ($setting->type == 'text')
+        {{ Form::label('value', 'Value') }}
+        {{ form_error('value', $errors) }}
+        {{ Form::text('value') }}
+      @elseif ($setting->type == 'boolean')
+        <label class="control checkbox">
+          {{ Form::checkbox('value', null, true) }}
+          <span class="control-indicator"></span>
+          Enabled
+        </label>
+        {{ form_error('value', $errors) }}
+      @else
+        <div class="messages -inline">This setting can't be changed. :(</div>
+      @endif
 
       {{ Form::submit('Update Setting', ['class' => 'btn']) }}
     {{ Form::close() }}
