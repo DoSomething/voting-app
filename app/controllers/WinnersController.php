244fe1aee9a11828769525046fb59d72c2ebef33
Andrea Gaither <angaither@gmail.com>
2014-12-18 10:39:50 -0500

    Added route and controller.

---
+++ app/controllers/WinnersController.php
@@ -0,0 +1,86 @@
+<?php
+
+class WinnersController extends \BaseController {
+
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index()
+  {
+
+  }
+
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create()
+  {
+
+  }
+
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store()
+  {
+
+  }
+
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function show(Candidate $candidate)
+  {
+
+  }
+
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit(Candidate $candidate)
+  {
+
+  }
+
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update(Candidate $candidate)
+  {
+
+  }
+
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy(Candidate $candidate)
+  {
+
+  }
+
+
+}
