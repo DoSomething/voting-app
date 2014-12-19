3a93a5e7c2b045d4884a1cb8389f9c06a2a31443
Andrea Gaither <angaither@gmail.com>
2014-12-19 15:57:29 -0500

    Match on id instead of name.

--- app/models/Winner.php
+++ app/models/Winner.php
@@ -32,16 +32,11 @@ class Winner extends Eloquent {
       $winners = DB::table('winners')
                     ->join('candidates', 'winners.candidate_id', '=', 'candidates.id')
                     ->join('categories', 'candidates.category_id', '=', 'categories.id')
-                    ->where('categories.name', '=', $category->name)
+                    ->where('categories.id', '=', $category->id)
                     ->select('winners.rank', 'winners.description', 'candidates.name', 'candidates.slug', 'candidates.photo', 'candidates.id')
                     ->orderBy('winners.rank')
                     ->get();
 
-      // $winners = Winner::with('candidate')
-      //             ->join('candidates as can', 'can.id', '=', 'winners.candidate_id')
-      //             ->join('categories as cat', 'cat.id', '=', 'can.category_id')
-      //             ->where('cat.id', $category->id)
-      //             ->orderBy('rank')->get();
       return $winners;
     }
     return;
