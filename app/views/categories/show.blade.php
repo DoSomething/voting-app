95ebca8a3a4c574860c571de01191aee4c9c4ef3
Andrea Gaither <angaither@gmail.com>
2014-12-19 15:53:13 -0500

    Set winner var to null, for scope/sanity.

--- app/views/categories/show.blade.php
+++ app/views/categories/show.blade.php
@@ -31,7 +31,7 @@
   </div>
 
   <script type="text/html" id="form-template">
-    @include('candidates.voteForm', ['category' => $category, 'candidate' => null])
+    @include('candidates.voteForm', ['category' => $category, 'candidate' => null, 'winner' => null])
   </script>
 
 @stop
