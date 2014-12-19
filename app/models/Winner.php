c8f231a0c008d22c91976023aadb5d539be45e10
Andrea Gaither <angaither@gmail.com>
2014-12-19 15:52:21 -0500

    Created get winners function.

--- app/models/Winner.php
+++ app/models/Winner.php
@@ -22,4 +22,29 @@ class Winner extends Eloquent {
     return $this->belongsTo('Candidate');
   }
 
+  /**
+   * Get the winners, given a category.
+   */
+  public static function getCategoryWinners(category $category)
+  {
+    $settings = App::make('SettingsRepository')->all();
+    if ($settings['show_winners']) {
+      $winners = DB::table('winners')
+                    ->join('candidates', 'winners.candidate_id', '=', 'candidates.id')
+                    ->join('categories', 'candidates.category_id', '=', 'categories.id')
+                    ->where('categories.name', '=', $category->name)
+                    ->select('winners.rank', 'winners.description', 'candidates.name', 'candidates.slug', 'candidates.photo', 'candidates.id')
+                    ->orderBy('winners.rank')
+                    ->get();
+
+      // $winners = Winner::with('candidate')
+      //             ->join('candidates as can', 'can.id', '=', 'winners.candidate_id')
+      //             ->join('categories as cat', 'cat.id', '=', 'can.category_id')
+      //             ->where('cat.id', $category->id)
+      //             ->orderBy('rank')->get();
+      return $winners;
+    }
+    return;
+
+  }
 }
