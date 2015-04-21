a43e0888088e84023e919a761cff5b3be3365c3c
Sergii Tkachenko <hi@sergii.org>
2015-04-21 18:12:25 +0300

    Seeds county_code for users.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -12,24 +12,28 @@ class UsersTableSeeder extends Seeder {
       'first_name' => 'Dave',
       'email' => 'dfurnes@dosomething.org',
       'password' => 'tops3cret',
+      'country_code' => 'US',
       ])->assignRole(1);
 
     User::create([
       'first_name' => 'Andrea',
       'email' => 'agaither@dosomething.org',
       'password' => 'tops3cret',
+      'country_code' => 'US',
       ])->assignRole(1);
 
     User::create([
       'first_name' => 'Calvin',
       'email' => 'cstowell@dosomething.org',
       'password' => 'tops3cret',
+      'country_code' => 'US',
       ])->assignRole(1);
 
     User::create([
       'first_name' => 'Naomi',
       'email' => 'nhirabayashi@dosomething.org',
       'password' => 'tops3cret',
+      'country_code' => 'US',
       ])->assignRole(1);
 
     if (App::environment('local')) {
@@ -38,6 +42,7 @@ class UsersTableSeeder extends Seeder {
           'first_name' => $faker->firstName,
           'email' => $faker->unique()->safeEmail,
           'birthdate' => $faker->date($format = 'm/d/Y', $max = 'now'),
+          'country_code' => $faker->countryCode(),
         ]);
       }
       foreach(range(1,25) as $index) {
@@ -45,6 +50,7 @@ class UsersTableSeeder extends Seeder {
           'first_name' => $faker->firstName,
           'phone' => $faker->unique()->phoneNumber,
           'birthdate' => $faker->date($format = 'm/d/Y', $max = 'now'),
+          'country_code' => $faker->countryCode(),
         ]);
       }
     }
