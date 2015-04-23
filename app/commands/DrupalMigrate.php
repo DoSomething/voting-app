c9ebb37b9d8553b6a2f9af3d0714aaf165559ce5
Sergii Tkachenko <hi@sergii.org>
2015-04-24 00:05:09 +0300

    Saves last processed id.
    So if error occurs, the script will continue from where it left of.

--- app/commands/DrupalMigrate.php
+++ app/commands/DrupalMigrate.php
@@ -47,47 +47,66 @@ class DrupalMigrate extends Command {
     ]);
 
     // Retrieve users.
-    $users = User::where('country_code', $this->argument('country_code'))->get();
+    $last_user = Setting::find('drupal_migrate_last_user');
+    if (!$last_user) {
+      Setting::create([
+        'key' => 'drupal_migrate_last_user',
+        'value' => -1,
+      ]);
+
+      // Reload the model.
+      // Fixes a bug when primary key gets reset to `0`.
+      $last_user = Setting::find('drupal_migrate_last_user');
+    }
+
+    $users = User::where('country_code', $this->argument('country_code'))
+      ->where('id', '>', $last_user->value)
+      ->orderBy('id', 'asc')
+      ->get();
+
     foreach($users as $key => $user) {
+
       if (empty($user->email)) {
         $this->error("User #" . $user->id . " skipped due to empty email.");
-        continue;
-      }
+      } else {
+        $fields =  [
+          'email'                    => $user->email,
+          'birthdate'                => $user->birthdate,
+          'first_name'               => $user->first_name,
+          'user_registration_source' => 'CGG',
+        ];
 
-      $fields =  [
-        'email'                    => $user->email,
-        'birthdate'                => $user->birthdate,
-        'first_name'               => $user->first_name,
-        'user_registration_source' => 'CGG',
-      ];
-
-      // Optional.
-      if (!empty($user->phone)) {
-        $fields['mobile'] = $user->phone;
-      }
-
-      try {
-        $this->client->post('users', [
-          'body' => json_encode($fields)
-        ]);
-        $this->info("User " . $user->email . " migrated.");
-      } catch (GuzzleHttp\Exception\ClientException $e) {
-        // Unknown error.
-        if (!$e->hasResponse()) {
-          $this->error($e);
-          continue;
+        // Optional.
+        if (!empty($user->phone)) {
+          $fields['mobile'] = $user->phone;
         }
 
-        $reason = $e->getResponse()->getReasonPhrase();
-        if (preg_match('/^: Email .+ is registered to User uid [0-9]+.$/', $reason)) {
-          // User already exists.
-          $this->comment("User " . $user->email . " already registered.");
-          continue;
+        try {
+          $this->client->post('users', [
+            'body' => json_encode($fields)
+          ]);
+          $this->info("User " . $user->email . " migrated.");
+        } catch (GuzzleHttp\Exception\ClientException $e) {
+          // No response, unknown error.
+          if (!$e->hasResponse()) {
+            $this->error($e);
+          } else {
+            // Check the response.
+            $reason = $e->getResponse()->getReasonPhrase();
+            $pattern = '/^: Email .+ is registered to User uid [0-9]+.$/';
+            if (preg_match($pattern, $reason)) {
+              // User already exists.
+              $this->comment("User " . $user->email . " already registered.");
+            } else {
+              // Unknown response.
+              $this->error($reason);
+            }
+          }
         }
-
-        // Unknown response.
-        $this->error($reason);
       }
+
+      // Save last_user id.
+      $last_user->update(['value' => $user->id]);
     }
   }
 
