89d23c4f8925a4f34823ecd19cdcd1ebfd75e7a8
David Furnes <david@dfurnes.com>
2015-04-27 10:53:16 -0400

    Add analytics setting.

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -1,17 +1,19 @@
 {{-- Google Analytics snippet --}}
-<script>
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
+@if($settings['google_analytics'])
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
                 m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-    ga('create', 'UA-56907042-1', 'auto');
-    ga('send', 'pageview');
-</script>
+        ga('create', '{{ $settings['google_analytics'] }}', 'auto');
+        ga('send', 'pageview');
+    </script>
+@endif
