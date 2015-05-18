84c336458fa03760863cd17a49445003ec9f5d2d
David Furnes <david@dfurnes.com>
2015-05-18 10:28:48 -0400

    Add CSRF token meta tag.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,11 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <title>@yield('title', $settings['site_title'])</title>
 
     <link rel="stylesheet" href="/css/app.css" type="text/css" media="screen" charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
