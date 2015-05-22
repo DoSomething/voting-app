f0e8d0f8e42c5668494eb8fbe3fe762da718e9a4
David Furnes <david@dfurnes.com>
2015-05-22 16:42:20 -0400

    Add write-in email setting.

---
+++ database/migrations/2015_05_22_203911_add_writein_email_to_settings.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWriteinEmailToSettings extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('settings')->insert([
+            'key' => 'writein_email',
+            'type' => 'text',
+            'description' => 'Email provided for write-in candidates.',
+            'value' => 'writein@voting.app'
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('settings')->delete(['key' => 'ui_tint']);
+    }
+
+}
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -36,7 +36,7 @@
 
         <p>
             If you know a {{ setting('candidate_type') }} who's done kickass things in the world, but don't see them
-            on our list, let us know by emailing <a href="mailto:writein@celebsgonegood.com">writein@celebsgonegood.com</a>.
+            on our list, let us know by emailing <a href="mailto:{{ setting('writein_email') }}">{{ setting('writein_email') }}</a>.
             Make sure to include the work they've done in the past year for social good (in 140 characters or less). Thank you!
         </p>
     </div>
