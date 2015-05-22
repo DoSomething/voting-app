a95af800e5944684585e1dc3d54bc1cf203708da
David Furnes <david@dfurnes.com>
2015-05-22 16:38:43 -0400

    Add UI tint setting.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,15 +20,33 @@
     <![endif]-->
 
     <style>
-        html {
-            background-image: url('{{ background('regular', 'none') }}');
+        html { background-image: url('{{ background('regular', 'none') }}'); }
+        @media (min-width: 1200px) {
+            html { background-image: url('{{ background('retina', 'none') }}'); }
         }
 
-        @media (min-width: 1200px) {
-            html {
-                background-image: url('{{ background('retina', 'none') }}');
+        @if(setting('ui_tint'))
+            h1.highlighted, h2.highlighted, h3.highlighted { background-color: {{ setting('ui_tint') }} !important; }
+
+            a, a:hover, a:active { color: {{ setting('ui_tint') }}; }
+            .button.-primary, .button.-primary:hover, .button.-primary:active {
+                color: #000 !important;
+                background-color: {{ setting('ui_tint') }} !important;
             }
-        }
+            .button.-secondary, .button.-secondary:hover, .button.-secondary:active {
+                color: {{ setting('ui_tint') }} !important;
+                background-color: transparent !important;
+            }
+            .button.-round, .button.-round:hover, .button.-round:active { background-color: {{ setting('ui_tint') }} !important; }
+
+            input[type="email"]:focus, input[type="number"]:focus, input[type="password"]:focus,
+            input[type="search"]:focus, input[type="tel"]:focus, input[type="text"]:focus,
+            input[type="url"]:focus, input[type="color"]:focus, input[type="date"]:focus,
+            input[type="datetime"]:focus, input[type="datetime-local"]:focus, input[type="month"]:focus,
+            input[type="time"]:focus, input[type="week"]:focus, textarea:focus {
+                border-color: {{ setting('ui_tint') }} !important;
+            }
+        @endif
     </style>
 
     @include('partials.metadata')
