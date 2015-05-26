50769cfba2be96638acc7fed9dd37efc113b31b9
David Furnes <david@dfurnes.com>
2015-05-26 10:49:14 -0400

    Use bcrypt helper function to hash passwords.

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      */
     public function setPasswordAttribute($password)
     {
-        $this->attributes['password'] = \Hash::make($password);
+        $this->attributes['password'] = bcrypt($password);
     }
 
     /**
