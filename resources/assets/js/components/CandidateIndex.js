8303c12f6c0d8a13279c9f5a292f0d6a6fab1ff5
David Furnes <david@dfurnes.com>
2015-06-05 12:28:22 -0400

    Add search field & filter based on name.

--- resources/assets/js/components/CandidateIndex.js
+++ resources/assets/js/components/CandidateIndex.js
@@ -1,21 +1,50 @@
 import React from 'react/addons';
 import Gallery from './Gallery';
+import SearchForm from './SearchForm';
 
-class CandidateIndex extends React.Component {
+const CandidateIndex = React.createClass({
+
+  getInitialState() {
+    return {
+      query: ''
+    }
+  },
+
+  filterCandidates(query) {
+    console.log(query);
+
+    this.setState({ query: query });
+  },
 
   render() {
+    const query = this.state.query.toUpperCase();
+
     var galleries = this.props.categories.map(function(category) {
+      // Filter candidates by search query...
+      let candidates = category.candidates;
+      if(query !== '') {
+        candidates = candidates.filter(function(candidate) {
+          const name = candidate.name.toUpperCase();
+          return (name.includes(query) ? candidate : null);
+        });
+      }
+
       return (
         <div key={category.id} className='category'>
           <h2 className='gallery-heading'>{category.name}</h2>
-          <Gallery items={category.candidates} />
+          <Gallery items={candidates} />
         </div>
       );
     });
 
-    return <div>{galleries}</div>;
+    return (
+      <div>
+        <SearchForm onChange={this.filterCandidates} />
+        {galleries}
+      </div>
+    );
   }
 
-};
+});
 
 export default CandidateIndex;
---
+++ resources/assets/js/components/SearchForm.js
@@ -0,0 +1,21 @@
+import React from 'react/addons';
+
+class SearchForm extends React.Component {
+
+  onChange(event) {
+    event.preventDefault();
+
+    this.props.onChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="search" placeholder='Find a candidate...' onChange={this.onChange.bind(this)} />
+      </div>
+    );
+  }
+
+}
+
+export default SearchForm;
