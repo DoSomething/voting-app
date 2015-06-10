159e0796bad6850ce1eaced4c42f62a29f3e3f41
David Furnes <david@dfurnes.com>
2015-06-10 10:25:53 -0400

    Remove old lazy loading code.

--- resources/views/candidates/partials/tile.blade.php
+++ resources/views/candidates/partials/tile.blade.php
@@ -5,9 +5,7 @@
             <div class="tile__meta">
                 <h1>{{ $candidate->name }}</h1>
             </div>
-            <img alt="{{ $candidate->name }}"
-                 src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
-                 data-src="{{ $candidate->thumbnail }}" width="265" height="265"/>
+            <img alt="{{ $candidate->name }}" src="{{ $candidate->thumbnail }}" width="265" height="265"/>
 
             @if(setting('enable_voting'))
                 <span class="button -round tile__action">Vote</span>
