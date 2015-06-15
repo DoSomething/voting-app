4b77646c64e226067bfdccea0f24b17875694d84
David Furnes <david@dfurnes.com>
2015-06-15 10:47:17 -0400

    Catch exceptions if React renderer process is unavailable.

--- app/Services/ReactService.php
+++ app/Services/ReactService.php
@@ -38,8 +38,13 @@ class ReactService
         $propsJSON = json_encode($props);
         $id = $component . md5($component . $propsJSON);
 
-        $response = $this->client->post($component, ['body' => $propsJSON]);
-        $renderedComponent = $response->getBody()->getContents();
+        try {
+            $response = $this->client->post($component, ['body' => $propsJSON]);
+            $renderedComponent = $response->getBody()->getContents();
+        } catch(RequestException $e) {
+            app('log')->error('Unable to pre-render React view.', [$e]);
+            $renderedComponent = '';
+        }
 
         $markup = '<div data-rendered-component="' . $component .'" id="' . $id . '">' . $renderedComponent . '</div>';
         $markup = $markup . '<script id="' . $id . '-props" type="application/json">' . $propsJSON . '</script>';
