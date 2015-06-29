1f5bc9d7229ffe8e1c31fd82ccd58df5fa18397b
David Furnes <david@dfurnes.com>
2015-06-29 15:13:19 -0400

    Add basic test coverage for US voters, and validating required fields.

--- tests/VotingTest.php
+++ tests/VotingTest.php
@@ -15,19 +15,6 @@ class VotingTest extends TestCase
         parent::setUp();
 
         $this->candidate = Candidate::all()->first();
-
-        $this->userEmail = 'test-example-user@example.com';
-    }
-
-    protected function fillVoteForm($candidate)
-    {
-        $this->visit(route('candidates.show', [$candidate->slug]))
-             ->fill('Puppet', '#first_name')
-             ->fill('1/1/1990', '#birthdate')
-             ->fill($this->userEmail, '#email')
-             ->press('Count My Vote');
-
-        return $this;
     }
 
     /**
@@ -36,11 +23,16 @@ class VotingTest extends TestCase
      */
     public function testSubmitVote()
     {
-        $this->fillVoteForm($this->candidate);
+        $this->visit(route('candidates.show', [$this->candidate->slug]))
+            ->fill('Puppet', '#first_name')
+            ->fill('1/1/1990', '#birthdate')
+            ->fill('test-example-user@example.com', '#email')
+            ->press('Count My Vote');
+
         $this->see('Thanks, we got that vote!');
 
         // Check the user & vote were created in the database
-        $user = User::where('email', $this->userEmail)->first();
+        $user = User::where('email', 'test-example-user@example.com')->first();
         $this->seeInDatabase('votes', [
             'user_id' => $user->id,
             'candidate_id' => $this->candidate->id
@@ -48,8 +40,51 @@ class VotingTest extends TestCase
 
         // If the user tries to vote again, they should see an error
         $this->visit('logout');
-        $this->fillVoteForm($this->candidate);
+
+        $this->visit(route('candidates.show', [$this->candidate->slug]))
+            ->fill('Puppet', '#first_name')
+            ->fill('1/1/1990', '#birthdate')
+            ->fill('test-example-user@example.com', '#email')
+            ->press('Count My Vote');
+
         $this->see('You already voted today!');
+    }
+
+    /**
+     * Test voting as a US user, where phone number is required.
+     */
+    public function testDomesticVote()
+    {
+        $url = route('candidates.show', [$this->candidate->slug]);
+
+        $this->visitFromCountry($url, 'US')
+            ->fill('Puppet', '#first_name')
+            ->fill('1/1/1992', '#birthdate')
+            ->fill('(123) 456-5555', '#phone')
+            ->press('Count My Vote');
+
+        $this->see('Thanks, we got that vote!');
+    }
+
+    /**
+     * Verify that required fields are validating.
+     */
+    public function testValidation()
+    {
+        $url = route('candidates.show', [$this->candidate->slug]);
+        $this->visitFromCountry($url, 'ES')
+            ->press('Count My Vote');
+
+        $this->see('The first name field is required.');
+        $this->see('The birthdate field is required.');
+        $this->see('The email field is required.');
+
+        $this->visitFromCountry($url, 'US')
+            ->press('Count My Vote');
+
+        $this->see('The first name field is required.');
+        $this->see('The birthdate field is required.');
+        $this->see('The phone field is required.');
     }
 
 }
