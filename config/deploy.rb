83c963dc9cb397e29036b8b11609ffa6a9cce3c4
Morgan Rich <mrich@dosomething.org>
2015-06-29 10:40:31 -0400

    Adding react render

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,9 +43,13 @@ namespace :deploy do
     run "cd #{release_path} && php artisan cache:clear"
   end
 
+  task :react_render do
+    run "forever stopall && forever start ../bootstrap/react_server.js"
+  end
+
 end
 
 after "deploy:update", "deploy:cleanup"
 after "deploy:symlink", "deploy:link_folders"
 after "deploy:link_folders", "deploy:artisan_migrate"
-after "deploy:artisan_migrate", "deploy:artisan_cache_clear"
+after "deploy:artisan_migrate", "deploy:react_render,deploy:artisan_cache_clear"
