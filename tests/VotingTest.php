01015bf249734961d34a89a3528b45fcb4b95a96
David Furnes <david@dfurnes.com>
2015-06-29 10:46:13 -0400

    Test that vote is created in database.

--- tests/VotingTest.php
+++ tests/VotingTest.php
@@ -1,18 +1,30 @@
 <?php
 
 use VotingApp\Models\Candidate;
+use VotingApp\Models\User;
 
 class VotingTest extends TestCase
 {
 
-    protected function fillVoteForm()
-    {
-        $candidate = Candidate::all()->first();
+    protected $candidate;
 
+    protected $userEmail;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->candidate = Candidate::all()->first();
+
+        $this->userEmail = 'test-example-user@example.com';
+    }
+
+    protected function fillVoteForm($candidate)
+    {
         $this->visit(route('candidates.show', [$candidate->slug]))
              ->fill('Puppet', '#first_name')
              ->fill('1/1/1990', '#birthdate')
-             ->fill('test-example-user@example.com', '#email')
+             ->fill($this->userEmail, '#email')
              ->press('Count My Vote');
 
         return $this;
@@ -24,12 +36,19 @@ class VotingTest extends TestCase
      */
     public function testSubmitVote()
     {
-        $this->fillVoteForm();
+        $this->fillVoteForm($this->candidate);
         $this->see('Thanks, we got that vote!');
+
+        // Check the user & vote were created in the database
+        $user = User::where('email', $this->userEmail)->first();
+        $this->seeInDatabase('votes', [
+            'user_id' => $user->id,
+            'candidate_id' => $this->candidate->id
+        ]);
 
         // If the user tries to vote again, they should see an error
         $this->visit('logout');
-        $this->fillVoteForm();
+        $this->fillVoteForm($this->candidate);
         $this->see('You already voted today!');
     }
 
