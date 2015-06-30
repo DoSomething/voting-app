1954dab39f8af1742f115c6fe2ad093ca6af8979
David Furnes <david@dfurnes.com>
2015-06-30 16:03:20 -0400

    Update category admin page copy.

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -5,8 +5,7 @@
         <div class="row">
             <h1 class="highlighted">All Categories</h1>
 
-            <p>These are all categories in the database. Categories will automatically show up in the site navigation
-                above.</p>
+            <p>These are all categories in the database. Each candidate may be in one category.</p>
         </div>
 
         <table>
