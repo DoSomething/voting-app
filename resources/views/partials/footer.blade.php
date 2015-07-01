31bca95582beb27b258bb3e4cba90505e48444df
David Furnes <david@dfurnes.com>
2015-07-01 11:29:40 -0400

    Add setting to control language used when sharing from footer.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,7 +6,7 @@
         <li><a class="social-icon -facebook js-share-link" href="{{ facebook_intent(url('/')) }}"><span>Facebook</span></a>
         </li>
         <li><a class="social-icon -twitter js-share-link"
-               href="{{ tweet_intent('.@dosomething\'s #CelebsGoneGood is back, vote for your fav celeb doing kickass things in the world this year now: ', url('/')) }}"><span>Twitter</span></a>
+               href="{{ tweet_intent(setting('twitter_site_language'), url('/')) }}"><span>Twitter</span></a>
         </li>
     </ul>
 </footer>
