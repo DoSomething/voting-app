c80a13eb3e0f6118e2f748789f18733beeb5959c
David Furnes <david@dfurnes.com>
2015-11-25 10:55:13 -0500

    Fix validation message. Since the message was "stored" when the custom validation is registered, it was always showing "DD/MM/YYYY". Instead, we'll use a custom replacer to ensure we get the right format for the user. Also adds some relevant tests for this!

--- app/Providers/ValidationServiceProvider.php
+++ app/Providers/ValidationServiceProvider.php
@@ -23,15 +23,21 @@ class ValidationServiceProvider extends ServiceProvider
     {
         $this->validator = $this->app->make('validator');
 
+        // Add custom validator for phone numbers
         $this->validator->extend('phone', function ($attribute, $value, $parameters) {
             $phoneRegex = '/^(?:\+?([0-9]{1,3})([\-\s\.]{1})?)?\(?([0-9]{3})\)?(?:[\-\s\.]{1})?([0-9]{3})(?:[\-\s\.]{1})?([0-9]{4})/';
 
             return preg_match($phoneRegex, $value);
         }, 'The :attribute must be a valid phone number.');
 
+        // Add custom validator for localized date (e.g. `MM/DD/YYYY` or `DD/MM/YYYY`).
         $this->validator->extend('localized_date', function ($attribute, $value, $parameters) {
             return LocalizedDate::validate($value);
-        }, 'Enter your :attribute '.LocalizedDate::getExpectedFormat().'.');
+        }, 'Enter your :attribute in :expected_date_format.');
+
+        $this->validator->replacer('localized_date', function ($message, $attribute, $rule, $parameters) {
+            return str_replace(':expected_date_format', LocalizedDate::getExpectedFormat(), $message);
+        });
     }
 
     /**
--- tests/VotingTest.php
+++ tests/VotingTest.php
@@ -123,8 +123,59 @@ class VotingTest extends TestCase
     }
 
     /**
+     * Verify basic form validation.
+     * @test
+     */
+    public function testSharedValidationRules()
+    {
+        $url = route('candidates.show', [$this->candidate->slug]);
+
+        $this->inCountry('US')
+            ->visit($url)
+            ->press('Count My Vote');
+
+        // Required fields should throw errors if blank.
+        $this->see('The first name field is required.');
+        $this->see('The birthdate field is required.');
+        $this->see('The email field is required.');
+    }
+
+    /**
+     * Verify localized date validation rules for domestic
+     * users (e.g. expected `MM/DD/YYYY`).
+     */
+    public function testUSLocalizedDateValidationRules()
+    {
+        $url = route('candidates.show', [$this->candidate->slug]);
+
+        $this->inCountry('US')
+            ->visit($url)
+            ->type('30/1/1990', 'birthdate') // Incorrectly DD/MM/YYYY date
+            ->press('Count My Vote');
+
+        $this->see('Enter your birthdate in MM/DD/YYYY.');
+    }
+
+    /**
+     * Verify localized date validation rules for international
+     * users (e.g. expected `DD/MM/YYYY`).
+     */
+    public function testInternationalLocalizedDateValidationRules()
+    {
+        $url = route('candidates.show', [$this->candidate->slug]);
+
+        $this->inCountry('ES')
+            ->visit($url)
+            ->type('1/30/1990', 'birthdate') // Incorrectly MM/DD/YYYY date
+            ->press('Count My Vote');
+
+        $this->see('Enter your birthdate in DD/MM/YYYY.');
+    }
+
+    /**
      * Verify that required fields are displayed & validated
-     * for international users.
+     * for international users in countries where we don't
+     * collect phone numbers.
      * @test
      */
     public function testInternationalValidation()
@@ -140,9 +191,11 @@ class VotingTest extends TestCase
 
         $this->press('Count My Vote');
 
-        $this->see('The first name field is required.');
-        $this->see('The birthdate field is required.');
-        $this->see('The email field is required.');
+        // Date field should require `DD/MM/YYYY` format.
+        $this->type('lol', 'birthdate');
+        $this->press('Count My Vote');
+
+        $this->see('Enter your birthdate in DD/MM/YYYY');
     }
 
     /**
