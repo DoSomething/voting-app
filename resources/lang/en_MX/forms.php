bc45588c282ae889d2919cfadaf42b89e0da136b
David Furnes <david@dfurnes.com>
2015-12-02 17:17:12 -0500

    Add appropriate placeholder text for MX users.

---
+++ resources/lang/en_MX/forms.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following are used to translate form field titles & placeholders.
+    |
+    */
+
+    'phone' => 'Mobile Number',
+    'phone_placeholder' => '52 155 55555555',
+
+];
