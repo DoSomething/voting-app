6b2b5ea0e2ae96df4950f3fe0288c223eb25ab61
katiecrane <katiethecrane@gmail.com>
2016-03-31 15:25:52 -0400

    add migration to create the new setting, only show vote button if voting is open and we select for it to be shown

---
+++ database/migrations/2016_03_31_143404_add_vote_button_setting.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVoteButtonSetting extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('settings')->insert([
+            'key' => 'vote_button',
+            'type' => 'boolean',
+            'value' => '1',
+            'description' => 'True if the vote button is to be displayed',
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('settings')->delete(['key' => 'vote_button']);
+    }
+}
