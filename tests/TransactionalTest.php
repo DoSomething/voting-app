c528b925c1ddbe50dfcc1fc208de0b80dc9a624a
David Furnes <david@dfurnes.com>
2016-04-28 13:54:20 -0400

    Add a bit more test coverage for transcationals being triggered.

---
+++ tests/TransactionalTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use VotingApp\Models\Candidate;
+use VotingApp\Models\User;
+use VotingApp\Models\Vote;
+
+class TransactionalTest extends TestCase
+{
+    /**
+     * Test that transactional payload is created correctly.
+     */
+    public function testMessageBrokerCall()
+    {
+        $user = User::create([
+            'first_name' => 'Jubilation',
+            'birthdate' => '10-5-1989',
+            'email' => 'jubilation.lee@marvel.com',
+        ]);
+
+        $candidate = Candidate::all()->first();
+
+        $vote = Vote::create([
+            'user_id' => $user->id,
+            'candidate_id' => $candidate->id,
+        ]);
+
+        // This should call the Message Broker with the expected activity type & routing key.
+        $broker = $this->mock(\VotingApp\Services\MessageBroker::class);
+        $broker->shouldReceive('publish')->with('vote', Mockery::any(), 'votingapp.event.vote')->once();
+
+        event(new \VotingApp\Events\UserCastFirstVote($vote));
+    }
+}
