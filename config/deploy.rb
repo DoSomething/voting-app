ae242babe58bfdac0d79f4b6fdb2cd13d4025efb
David Furnes <david@dfurnes.com>
2016-04-29 14:35:59 -0400

    Don't symlink a path we don't use.
    Thumbnails were moved into the “public/images” directory so this is no
    longer necessary.

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ namespace :deploy do
   task :link_folders do
     run "ln -nfs #{shared_path}/.env #{release_path}/"
     run "ln -nfs #{shared_path}/images #{release_path}/public"
-    run "ln -nfs #{shared_path}/thumbnails #{release_path}/public"
+
     folders.each do |folder|
       run "ln -nfs #{shared_path}/#{folder} #{release_path}/storage/#{folder}"
     end
